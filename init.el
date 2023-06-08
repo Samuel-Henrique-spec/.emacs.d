a34092366bd0167f7bc433c29e051ceb653f9959
-Samuca- <78044674+Samuel-Henrique-spec@users.noreply.github.com>
2023-06-08 15:58:25 -0300

    tab modificado

--- init.el
+++ init.el
@@ -4,13 +4,16 @@
 ;;Adicionar gruber-darker-theme
 ;;Adicionar csharp-mode
 
+;;Tab config
+(setq-default tab-width 2)
+
 ;; Remove welcome message
 (setq inhibit-startup-message t)
 
 ;; Remove menus
-;;(menu-bar-mode -1)
-;;(scroll-bar-mode -1)
-;;(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
 ;;Retirei as configs pois uso o emacs terminal 
 
 ;;Números das linhas
@@ -66,7 +69,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(almost-mono-theme almost-mono-themes use-package)))
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+	 '("bddf21b7face8adffc42c32a8223c3cc83b5c1bbd4ce49a5743ce528ca4da2b6" default))
+ '(package-selected-packages
+	 '(csharp-mode gruber-darker-theme almost-mono-theme almost-mono-themes use-package))
+ '(warning-suppress-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
